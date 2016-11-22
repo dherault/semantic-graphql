caab96104e56b14c2aaa7ef0fc57d24ec9b09da3
David Hérault <dherault@gmail.com>
2016-11-22 20:22:52 +0100

    index resources in graph

--- src/Graph.js
+++ src/Graph.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
 const path = require('path');
+const { readFileSync } = require('fs');
 const createRdfParser = require('n3').Parser;
+const invariant = () => null;
 
 function parseRdf(data, options) {
   const parser = createRdfParser(options);
@@ -10,32 +11,48 @@ function parseRdf(data, options) {
   return parser.parse(data);
 }
 
+function upsert(theGuy, whoDid, what, kevinOffACliff) {
+  const theGuyWhoDid = theGuy[whoDid];
+
+  /*do you know*/theGuyWhoDid ?
+  theGuyWhoDid[what] ?
+  theGuyWhoDid[what].push(kevinOffACliff) :
+  theGuyWhoDid[what] = [kevinOffACliff] : // ?!!??!
+  theGuy[whoDid] = { [what]: [kevinOffACliff] };
+}
+
+const isValidIri = s => typeof s === 'string' && s.startsWith('http');
+
+function index(g, { subject, predicate, object }) {
+  invariant(isValidIri(subject) && isValidIri(predicate));
+
+  if (g[subject] && g[subject][predicate] && g[subject][predicate][object]) return;
+
+  upsert(g, subject, predicate, object);
+  if (isValidIri(object)) upsert(g, object, `_${predicate}`, subject); // for now
+  upsert(g, predicate, `__${subject}`, object);
+  upsert(g, predicate, `___${object}`, subject);
+}
+
+
+const g = {};
 const utf8 = 'utf-8';
-const rdfsTriples = parseRdf(fs.readFileSync(path.join(__dirname, '../ontologies/rdfs.ttl'), utf8));
-const rdfTriples = parseRdf(fs.readFileSync(path.join(__dirname, '../ontologies/rdf.ttl'), utf8));
-const owlTriples = parseRdf(fs.readFileSync(path.join(__dirname, '../ontologies/owl.ttl'), utf8));
-// const skosTriples = parseRdf(fs.readFileSync(path.join(__dirname, '../ontologies/skos.ttl'), utf8));
+const parseFileAndIndex = (g, l, e = utf8) => parseRdf(readFileSync(path.join(__dirname, l), e)).forEach(t => index(g, t));
+
+parseFileAndIndex(g, '../ontologies/rdfs.ttl');
+parseFileAndIndex(g, '../ontologies/rdf.ttl');
+parseFileAndIndex(g, '../ontologies/owl.ttl');
+parseFileAndIndex(g, '../ontologies/skos.ttl');
 
 class Graph {
 
   constructor() {
-    this.triples = [...rdfTriples, ...rdfsTriples, ...owlTriples];
-  }
+    Object.assign(this, g);
 
-  parse(data) {
-    const graphId = Math.random().toString();
-    this.triples.push(...parseRdf(data).map(x => Object.assign(x, { graph: graphId })));
+    this.addTriple = t => index(this, t);
+    this.parse = (d, o) => parseRdf(d, o).forEach(this.addTriple);
+    this.parseFile = (l, e = utf8) => this.parse(readFileSync(l, e));
   }
-
-  parseFile(location, encoding = utf8) {
-    this.parse(fs.readFileSync(location, encoding));
-  }
-
-  // getObjects(subject, predicate) {
-  //   return this.triples
-  //   .filter(t => t.subject === subject && t.predicate === predicate)
-  //   .map(t => t.object);
-  // }
 }
 
 module.exports = Graph;
