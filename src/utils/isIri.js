41c88a83e67d6cf6f53ccaf70e286accc14dae82
Astn <aharris@lifetimefitness.com>
2017-02-23 08:12:07 -0700

    Fixing issue #1 - IRI Check

--- src/utils/isIri.js
+++ src/utils/isIri.js
@@ -1,5 +1,8 @@
 function isIri(iri) {
-  return typeof iri === 'string' && iri.startsWith('http'); // very basic check
+  // According to: https://tools.ietf.org/html/rfc3987#section-2
+  // IRI            = scheme ":" ihier-part [ "?" iquery ] [ "#" ifragment ]
+  // every IRI requires a scheme followed by ":".
+  return typeof iri === 'string' && iri.includes(':'); 
 }
 
 module.exports = isIri;
